09a62118b698f028edc97a1c9bc64e4a65f9ce59
Marco Ricci <mr.ricci79@gmail.com>
2022-10-31 08:55:38 +0000

    post controller completed

--- 03-Models/app/controllers/posts_controller.rb
+++ 03-Models/app/controllers/posts_controller.rb
@@ -8,26 +8,50 @@ class PostsController
   def index
     # DO NOT WRITE SQL QUERIES
     # TODO: gather all posts from the database
+    posts = Post.all
     # TODO: give them to the view to be printed
+    @view.display(posts)
   end
 
   def create
-    # DO NOT WRITE SQL QUERIES
-    # TODO: create a post
+    title = @view.ask_for(:title)
+    url = @view.ask_for(:url)
+    post = Post.new(title: title, url: url)
+    post.save
   end
 
   def update
     # DO NOT WRITE SQL QUERIES
     # TODO: update a post
+    posts = Post.all
+    @view.display(posts)
+    id = @view.ask_for(:id).to_i
+    post = Post.find(id)
+    title = @view.ask_for(:title)
+    url = @view.ask_for(:url)
+    post.title = title
+    post.url = url
+    post.save
   end
 
   def destroy
     # DO NOT WRITE SQL QUERIES
     # TODO: destroy a post
+    posts = Post.all
+    @view.display(posts)
+    id = @view.ask_for(:id).to_i
+    post = Post.find(id)
+    post.destroy
   end
 
   def upvote
     # DO NOT WRITE SQL QUERIES
     # TODO: increment the `votes` column for a post
+    posts = Post.all
+    @view.display(posts)
+    id = @view.ask_for(:id).to_i
+    post = Post.find(id)
+    post.votes += 1
+    post.save
   end
 end
--- 03-Models/app/views/posts_view.rb
+++ 03-Models/app/views/posts_view.rb
@@ -1,3 +1,14 @@
 class PostsView
   # TODO: implement some methods here when controller needs to `puts` or `gets`
+  def display(posts)
+    posts.each do |post|
+      puts "#{post.id}. #{post.title} - #{post.votes} votes - #{post.url}"
+    end
+  end
+
+  def ask_for(label)
+    puts "#{label}?"
+    print ">"
+    gets.chomp
+  end
 end
