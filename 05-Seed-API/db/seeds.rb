53b5aafdd26f77b10f301d97dc50ea24c926e10c
Marco Ricci <mr.ricci79@gmail.com>
2022-10-31 09:26:20 +0000

    seed file completed

--- 05-Seed-API/db/seeds.rb
+++ 05-Seed-API/db/seeds.rb
@@ -2,3 +2,12 @@ require "json"
 require "rest-client"
 
 # TODO: Write a seed to insert 10 posts in the database fetched from the Hacker News API.
+response = RestClient.get "https://hacker-news.firebaseio.com/v0/topstories.json"
+posts_ids = JSON.parse(response)
+
+posts_ids.take(10).each do |post|
+  res = RestClient.get "https://hacker-news.firebaseio.com/v0/item/#{post}.json"
+  post = JSON.parse(res)
+  new_post = Post.new(title: post["title"], url: post["url"], votes: post["score"])
+  new_post.save
+end
